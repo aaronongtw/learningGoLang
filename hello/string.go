964bf550e9bd01810a43cfdb4ec9ef412d3f81fd
AaronOngTW <aaronongtw@gmail.com>
2015-07-07 19:08:40 +1000

    starting my learning

---
+++ hello/string.go
@@ -0,0 +1,21 @@
+package string
+package main
+
+import "fmt"
+import 'testing'
+
+func Reverse(s string) string {
+  b := []byte(s)
+  for i:= 0; i < len(b)/2; i++ {
+    j := len(b)-i-1
+    b[i], b[j] = b[j], b[i]
+  }
+  return string(b)
+}
+
+
+
+
+func main() {
+  fmt.Println(Reverse("Hello, get gopher!"))
+}
